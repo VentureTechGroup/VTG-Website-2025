c06edc932fa949e8b4b8f498fb7ae5b499ecb749
rfilbin <49924792+rfilbin@users.noreply.github.com>
2025-08-22 11:32:50 -0400

    added AF Embedded Deployment code
    Did a quick and dirty copy paste of the agent code into the website to see if it works

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,26 @@ export default function RootLayout({ children }) {
         <body>
           <GTM.NoScript />
           {children}
+          <script type='text/javascript'>
+          	function initEmbeddedMessaging() {
+          		try {
+          			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+          
+          			embeddedservice_bootstrap.init(
+          				'00DEi00000COEFr',
+          				'VTG_Website_Messaging_Setting',
+          				'https://venturetechgroup--uat.sandbox.my.site.com/ESWVTGWebsiteMessaging1754648854815',
+          				{
+          					scrt2URL: 'https://venturetechgroup--uat.sandbox.my.salesforce-scrt.com'
+          				}
+          			);
+          		} catch (err) {
+          			console.error('Error loading Embedded Messaging: ', err);
+          		}
+          	};
+          </script>
+          <script type='text/javascript' src='https://venturetechgroup--uat.sandbox.my.site.com/ESWVTGWebsiteMessaging1754648854815/assets/js/bootstrap.min.js' onload='initEmbeddedMessaging()'></script>
+
         </body>
       </html>
     </>
